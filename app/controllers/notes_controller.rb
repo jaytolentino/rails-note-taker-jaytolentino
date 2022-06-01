3fa41cd8e6e8e05a0bef544203563b9e1d5af667
Jay Tolentino <jltolent@gmail.com>
2022-05-31 18:19:58 -0700

    add index to notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class NotesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    query = params[:query]
+
+    @notes = if query.present?
+      Note.where('title LIKE ?', "%#{query}%").all
+    else
+      Note.all
+    end
+
+    render json: @notes
+  end
 end
